b1a42d4e717aa69cc8d743c3fcee3984a471f2d6
Brooke F <brooke.floyd10@gmail.com>
2026-06-27 21:59:42 -0500

    added to do list component

---
+++ static-todo-react-app/src/components/Header.jsx
@@ -0,0 +1,4 @@
+const Header = () => {
+   return( <h1>To Do List</h1>
+   )
+}
---
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -0,0 +1,14 @@
+const ToDoList = () => {
+    return (
+        <>
+        <h3>To Do List:</h3>
+        <ul>
+            <li>Learn React</li>
+            <li> Build a project</li>
+            <li>Read documentation</li>
+        </ul>
+    </>
+    );
+}
+
+export default ToDoList;
